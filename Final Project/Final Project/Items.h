dc6dd843fe85393bad8458236b5ed4642bb15f8d
xandercasi541389-bit <xandercasi541389@gmail.com>
2026-04-05 21:48:42 -0400

    Update Items.h

--- Final Project/Final Project/Items.h
+++ Final Project/Final Project/Items.h
@@ -1,58 +1,121 @@
-
+#pragma once
 #include <string>
 #include <iostream>
 
-//top class Items
+/*
+This header file is for defining items for the game
+If you want to use this file add #include "item.h" to the .cpp file
+When you want to import equipment from this file to the main, add (Sword. //name of sword//;)
+                                                                  (Sheild. //name of sheild//;)
+
+When you want to equip an item in the main, add (//name of sheild//.equipDefense(////name of defense stat))
+                                                (//name of sword//.equipAttack(////name of attack stat))
+
+when you want to import the health potion just add (HPotion potion;)
+When you want to use the health potion just add (potion.use(health);)
+*/
+
+
+
+
+// top class Items
 class Items{
 private:
 	std::string name;
 public:
 	Items(std::string n) : name(n) {}
-	virtual ~Items() {}
-
-	virtual void use(int& health) = 0;
+	virtual ~Items() = default;
+	virtual std::string getName() const = 0;
+	// equipment and consumables have different behavior. Provide two
+	// distinct no-op hooks so subclasses can override only what they need.
+	virtual void equipDefense(int& /*defense*/) { }
+	virtual void equipAttack(int& /*attack*/) { }
 };
 
 //subclass Equipment and consumables
 class Equipment : public Items {
 private:
-	int durability;
+	int durability{100};
 
 public:
-	
-
+	Equipment(std::string n) : Items(n) {}
 };
 
 class consumables : public Items {
 public:
 	consumables(std::string n) : Items(n) {}
 
+	// consumables can be used to affect health
+	virtual void use(int& health) = 0;
 };
 
 //subclass of equipment sheild and sword
-class Sheild : public Equipment {
-private: 
-	//how much the sheild will defend against incoming attack damage
-	int defend{4};
-public:
+class Shields : public Equipment {
+private:
+	// how much the shield will defend against incoming attack damage
+	int defend;
 
+	// simple counter to vary shield strength depending on creation order.
+	static inline int sh_count =0;
+public:
+	// Default constructor: always calls Equipment with a name
+	std::string getName() const override {
+		return "Shield";
+	}
+	Shields() : Equipment("weakshield") {
+		++sh_count;
+		if (sh_count == 1) defend = 4;      // weak
+		else if (sh_count == 2) defend = 7; // strong
+		else if (sh_count == 3) defend = 10; // stronger
+		else defend = 4; // default defense for new sheilds
+	}
+
+	void equipDefense(int& defense) override {
+		defense += defend;
+	}
 };
 
-class Sword : public Equipment {
-private: 
-	//how much the sword will increase attack damage
-	int attack{5};
-public:
+// Definition of the static counter. 
+//int Shields::sh_count = 0;
 
+
+class Swords : public Equipment {
+private:
+	// how much the sword will defend against incoming attack damage
+	int attack;
+
+	// simple counter to vary sword strength depending on creation order.
+	static inline int sw_count=0;
+public:
+	// Default constructor: always calls Equipment with a name
+	std::string getName() const override {
+		return "Sword";
+	}
+	Swords() : Equipment("weaksword") {
+		++sw_count;
+		if (sw_count == 1) attack = 4;      // weak
+		else if (sw_count == 2) attack = 7; // strong
+		else if (sw_count == 3) attack = 10; // stronger
+		else attack = 4; // default defense for new swords
+	}
+
+	void equipAttack(int& attack) override {
+		attack += attack;
+	}
 };
+
+// Definition of the static counter. 
+//int Swords::sw_count = 0;
 
 //subclass of consumables Healing Potion(HPotion)
 class HPotion : public consumables {
 private:
-	int healthregained;
+	int healthregained{7};
 public:
-	HPotion() : consumables("potion"), healthregained{7} {}
-	
+	HPotion() : consumables("potion") {}
+	std::string getName() const override {
+		return "HPotion";
+	}
 	void use(int& health) override {
 		health += healthregained;
 	}
