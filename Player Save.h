cf51e3fa1e47a2d21dfda106071b1d4466b8c119
xandercasi541389-bit <xandercasi541389@gmail.com>
2026-04-12 11:26:31 -0400

    Update Player Save.h

--- Player Save.h
+++ Player Save.h
@@ -2,13 +2,17 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <memory>
 #include <filesystem>
+#include "Items.h"
+#include "Room.h"
+
 
 struct Stats {
     int HP; //health points
     int ATT; //attack
     int DEF; //defense
-    Stats() :HP(0), ATT(0), DEF(0) {}
+    Stats() : HP(0), ATT(0), DEF(0) {}
     Stats(int hp, int att, int def) : HP(hp), ATT(att), DEF(def) {}
 };
 
@@ -16,11 +20,11 @@ class SaveData {
 public:
     SaveData();
 
-    void addItem(const std::string& item) {
-        m_inventory.push_back(item);
+    void addItem(std::unique_ptr<Items> item) {
+        m_inventory.push_back(std::move(item));
     }
 
-    const std::vector<std::string>& getCurrentInventory() const {
+    const std::vector<std::unique_ptr<Items>>& getCurrentInventory() const {
         return m_inventory;
     }
 
@@ -39,9 +43,8 @@ public:
     Stats getCurrentStats() const {
         return m_stats;
     }
-
 private:
-    std::vector<std::string> m_inventory;
+    std::vector<std::unique_ptr<Items>> m_inventory;
     Stats m_stats{};
     int m_room{0};
 };
@@ -50,11 +53,22 @@ private:
 
 class SaveSystem {
 public:
-
     SaveSystem();
-
+ 
 	bool loadSaveFile(const std::filesystem::path& file, SaveData& out); //Loads the save
 	bool deleteSaveFile(const std::filesystem::path& file); //deletes save file
+    void storeSaveName() {
+        std::string filename;
+        std::getline(std::cin, filename);
+        m_filename = filename; 
+    }
+    std::string getSaveName() {
+        return m_filename;
+    }
+    bool createSaveFile(const std::filesystem::path& file);
 	bool saveToFile(const std::filesystem::path& file, const SaveData& data) const ; //saves to the file
+
+private:
+    std::string m_filename;
 
 };
